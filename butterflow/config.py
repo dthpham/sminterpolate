f1906e1cf31c278c4015cbb93ef99ef4d28d1194
Duong Pham <dthpham@gmail.com>
2015-02-12 01:31:35 -0700

    config now subclasses dict for simplicity

--- butterflow/config.py
+++ butterflow/config.py
@@ -1,56 +1,35 @@
 import os
-import collections
 
 
-class SettingsDictionary(collections.MutableMapping):
-  '''A dictionary object of a configuration file with key and value
-  pairs in the form key=value, with at most one pair on each line.
-  Lines starting with a hash (#) are treated as comments.'''
+class Config(dict):
+  """Works exactly like a dict but can load a config file with key and
+  value pairs in the form `key = value` with at most one pair on each line.
+  Lines starting with a hash `#` are treated as comments.
+  """
+
   def __init__(self, *args, **kwargs):
-    self.store = dict()
-    self.update(dict(*args, **kwargs))
-    self.config_path = None
+    self.update(*args, **kwargs)
 
-  def __getitem__(self, key):
-    return self.store[self.__keytransform__(key)]
-
-  def __setitem__(self, key, value):
-    self.store[self.__keytransform__(key)] = value
-
-  def __delitem__(self, key):
-    del self.store[self.__keytransform__(key)]
-
-  def __iter__(self):
-    return iter(self.store)
-
-  def __len__(self):
-    return len(self.store)
-
-  def __keytransform__(self, key):
-    return key.lower()
-
-  def save(self):
-    if self.config_path is None:
-      return
-    with open(self.config_path, 'w') as f:
+  def to_file(self, path):
+    with open(path, 'w') as f:
       for k, v in self.iteritems():
         f.write('{}={}\n'.format(k, v))
 
   @classmethod
-  def from_settings_file(cls, path):
+  def from_file(cls, path):
+    """Returns a Config object updated from kv pairs from a file"""
     obj = cls()
-    obj.config_path = path
     with open(path, 'r') as f:
-      for line in f.readlines():
-        line = line.strip()
-        if line.startswith('#'):
+      for l in f.readlines():
+        l = l.strip()
+        if l.startswith('#'):
           continue
-        if '=' in line:
-          k, v = line.split('=')
-          obj[k] = v
+        if '=' in l:
+          k, v = l.split('=')
+          obj[k.lower()] = v.lower()
     return obj
 
 from __init__ import conf_path
 if not os.path.exists(conf_path):
   open(conf_path, 'a').close()
-settings = SettingsDictionary.from_settings_file(conf_path)
+settings = Config.from_file(conf_path)
