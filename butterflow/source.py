a85231d3b867195130eb8427880452e0b5e87845
Duong Pham <dthpham@gmail.com>
2015-10-02 15:27:43 -0700

    Fixed source self._idx returning a boolean

--- butterflow/source.py
+++ butterflow/source.py
@@ -15,14 +15,12 @@ class FrameSource(object):
         # `avinfo.get_av_info` returns since several calculations in
         # `render.py` are based upon it
         self.frames = 0  # total num of frames
-        self._idx = 0    # zero-based index of the frame to be read next
 
     def open(self):
         self.src = cv2.VideoCapture(self.path)  # open the file
         if not self.src.isOpened():
             raise RuntimeError('unable to open file')
         self.frames = int(self.src.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
-        self.idx = 0  # will seek to the first frame
 
     def close(self):
         if self.src is not None:
@@ -31,15 +29,10 @@ class FrameSource(object):
 
     @property
     def idx(self):
+        # zero-based index of the frame to be read next
         # videocapture will keep track of `idx` for us
         # return whatever it reports
         return self.src.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-
-    @idx.setter
-    def idx(self, idx):
-        # setting `idx` will do a seek
-        # TODO: do nothing if seeking to the current pos
-        self._idx = self.src.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, idx)
 
     def seek_to_frame(self, idx):
         # seek to frame that will be read next
@@ -47,7 +40,10 @@ class FrameSource(object):
             msg = 'seeked out of frame range [0,%s]'.format(self.frames - 1)
             raise IndexError(msg)
         # do seek, position will update automatically
-        self.idx = idx
+        rc = self.src.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, idx)
+        if rc is not True:
+            msg = 'unable to seek to frame at idx={}'.format(idx)
+            raise RuntimeError(msg)
 
     def read(self):
         # reads frame at `self.idx` and returns it
