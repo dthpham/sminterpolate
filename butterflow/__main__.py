38b43cf82a20e7a38c09941514e78a962a1c48bb
Duong Pham <dthpham@gmail.com>
2017-04-08 01:13:56 -0700

    Fix #41: add multiprocessing freeze support

--- butterflow/__main__.py
+++ butterflow/__main__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import multiprocessing
+
 
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
+
     import sys
     from butterflow.cli import main
     sys.exit(main())
